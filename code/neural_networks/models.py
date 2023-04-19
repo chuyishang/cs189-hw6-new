c62d2a49ef9ac43385c4386426e534701417b5a2
Chuyi Shang <chuyishang@berkeley.edu>
2023-04-18 18:40:07 -0700

    added predict

--- code/neural_networks/models.py
+++ code/neural_networks/models.py
@@ -340,5 +340,7 @@ class NeuralNetwork(ABC):
         ### YOUR CODE HERE ###
         # Do a forward pass. Maybe use a function you already wrote?
         # Get the loss. Remember that the `backward` function returns the loss.
-        return ...
+        Z = self.forward(X)
+        loss = self.loss.forward(Y, Z)
+        return Z, loss
 
